e94527d65e48c9a9aa51b55b89d51b4717d7ff22
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-04-19 23:30:45 +0200

    Fixed stale state within a closure

--- src/features/Timer/Timer.tsx
+++ src/features/Timer/Timer.tsx
@@ -1,34 +1,48 @@
-import React, { MouseEvent, useCallback, useRef, useState } from "react";
+import React, { MouseEvent, useCallback, useEffect, useRef, useState } from "react";
 import { Temporal } from "@js-temporal/polyfill";
 import { Circle } from "@/features/Timer/Circle";
 
 interface ITimerProps extends React.ComponentProps<"div"> {}
 
+const seconds = 60;
+
 export function Timer(props: ITimerProps) {
   const [isStarted, setIsStarted] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
-  const [startDate, setStartDate] = useState<Temporal.PlainTime>();
-  const [endDate, setEndDate] = useState<Temporal.PlainTime>();
+  // Using refs to mitigate stale state within a closure
+  const startDate = useRef<Temporal.PlainTime>();
+  const endDate = useRef<Temporal.PlainTime>();
+  const [timeLeft, setTimeLeft] = useState<Temporal.Duration>();
   const intervalRef = useRef<number>();
 
-  function advanceTimer() {
-    console.log(Temporal.Now.plainTimeISO().toString());
-  }
+  const secondsLeft = () => Math.floor(timeLeft?.total("seconds") ?? seconds);
+
+  const advanceTimer = () => {
+    if (!endDate.current) return;
+
+    setTimeLeft(
+      (timeElapsed) => (timeElapsed = Temporal.Now.plainTimeISO().until(endDate.current))
+    );
+  };
+
+  useEffect(() => {
+    if (secondsLeft() > 0) return;
+
+    clearInterval(intervalRef.current);
+  }, [timeLeft]);
 
   //#region EventHandlers
-  const onStartClick = useCallback(
-    (event: MouseEvent<HTMLButtonElement>) => {
-      setIsStarted(true);
+  const onStartClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setIsStarted(true);
 
-      const timeNow = Temporal.Now.plainTimeISO();
-      const sessionDuration = Temporal.Duration.from({ minutes: 25 });
-      setStartDate(timeNow);
-      setEndDate(timeNow.add(sessionDuration));
+    const timeNow = Temporal.Now.plainTimeISO();
+    const sessionDuration = Temporal.Duration.from({ seconds: seconds });
+    startDate.current = timeNow;
+    endDate.current = timeNow.add(sessionDuration);
 
-      intervalRef.current = setInterval(advanceTimer, 1000);
-    },
-    [intervalRef.current]
-  );
+    // TODO: Investigate 1-sec delay on start
+    intervalRef.current = setInterval(() => advanceTimer(), 1000);
+  };
 
   const onSkipClick = useCallback(
     (event: MouseEvent<HTMLButtonElement>) => {
@@ -71,7 +85,11 @@ export function Timer(props: ITimerProps) {
 
   return (
     <div {...props}>
-      <Circle showProgress={isStarted} progress={(20 * 60) / (25 * 60)} timeRemaining={"25:00"} />
+      <Circle
+        showProgress={isStarted}
+        progress={secondsLeft() / seconds}
+        timeRemaining={`${seconds}`}
+      />
       <div className="flex justify-evenly">
         {!isStarted && <button onClick={onStartClick}>start</button>}
         {(isStarted || isPaused) && <button onClick={onSkipClick}>skip</button>}
