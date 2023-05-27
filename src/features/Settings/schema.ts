2039e32a12f46f25f2c111e269e33635da60c128
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-05-27 23:37:59 +0200

    fix: Invalid minimum value for break duration

--- src/features/Settings/schema.ts
+++ src/features/Settings/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const settingsSchema = z.object({
   sessionDuration: z.coerce.number().min(1).max(120),
-  breakDuration: z.coerce.number().min(5).max(60),
+  breakDuration: z.coerce.number().min(1).max(60),
   longBreakDuration: z.coerce.number().min(1).max(60),
   sessionsBeforeLongBreak: z.coerce.number().min(1).max(10),
   dailyGoal: z.coerce.number().min(1).max(16),
