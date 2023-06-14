7c7c0a6702a6c7718321514c88b096638b7836bd
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-06-14 19:30:14 +0200

    fix: Commit forgotten change

--- src/store/nightOwlMiddleware.ts
+++ src/store/nightOwlMiddleware.ts
@@ -7,27 +7,6 @@ import { getNextMidnight } from "@/store/helpers";
 
 const LS_NEXT_MIDNIGHT_KEY = "next_midnight";
 
-/**
- * Compute next offset midnight.
- * @param time Temporal.PlainTime compatible time string.
- */
-export function getNextMidnightFromString(time: string) {
-  console.log(time);
-  const _time = Temporal.PlainTime.from(time);
-  return getNextMidnight(_time.hour, _time.minute);
-}
-
-/**
- * Compute next offset midnight.
- * @param offsetHours A number of hours to offset by.
- * @param offsetMinutes (optional) A number of minutes to offset by.
- */
-export function getNextMidnight(offsetHours: number, offsetMinutes?: number) {
-  return Temporal.Now.plainDateTime("gregory")
-    .round({ smallestUnit: "day", roundingMode: "trunc" })
-    .add({ days: 1, hours: offsetHours, minutes: offsetMinutes ?? 0 });
-}
-
 export function getStoredNextMidnight(): string | null {
   const stored = localStorage.getItem(LS_NEXT_MIDNIGHT_KEY);
   return stored ? stored : null;
