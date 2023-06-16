a9da0385fbacd1b331f45f96f57d2e198c75d3a9
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-06-16 13:05:05 +0200

    feat: Added confetti

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,15 +7,30 @@ import { useAppDispatch, useAppSelector } from "../store";
 import { PomodoroStatus } from "@/features/Timer/timerSlice";
 import { updateSettings } from "@/features/Settings/settingsSlice";
 import { isDarkMode, toggleTheme } from "@/common/darkMode";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
+import { loadFull } from "tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { confettiConfig } from "@/app/confettiConfig";
 
 export function App() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: Container | undefined) => {
+    // setTimeout(() => container?.pauseEmitter(), 2500);
+    // console.log(container);
+  }, []);
+
   const { isOpen, openPortal, closePortal } = usePortal(false);
   const timerStatus = useAppSelector((state) => state.timer.status);
   const appSettings = useAppSelector((state) => state.settings);
   const isSoundEnabled = useAppSelector((state) => state.settings.canPlaySound);
   const dispatch = useAppDispatch();
   const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(isDarkMode());
+
+  const condition = true;
 
   function toggleSound() {
     dispatch(updateSettings({ ...appSettings, canPlaySound: !isSoundEnabled }));
@@ -27,33 +42,44 @@ export function App() {
   }
 
   return (
-    <div className="relative grid h-screen justify-center transition-colors dark:bg-dark">
-      <Timer
-        className="absolute left-1/2 top-1/2"
-        style={{
-          transform: "translate(-50%, -50%)",
-        }}
-      />
+    <>
+      {condition && (
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          className="left-0, pointer-events-none absolute top-0 z-50 h-full w-full"
+          options={confettiConfig}
+        />
+      )}
+      <div className="relative grid h-screen justify-center transition-colors dark:bg-dark">
+        <Timer
+          className="absolute left-1/2 top-1/2"
+          style={{
+            transform: "translate(-50%, -50%)",
+          }}
+        />
 
-      <>
-        <div className="absolute right-0 top-0 m-6 flex gap-4">
-          <IconContext.Provider value={{ size: "2em", className: "dark:text-neutral-200" }}>
-            <button onClick={toggleAppTheme}>{isDarkModeEnabled ? <FiSun /> : <FiMoon />}</button>
-            <button onClick={toggleSound}>{isSoundEnabled ? <FiBell /> : <FiBellOff />}</button>
-            {timerStatus === PomodoroStatus.UNSTARTED && (
-              <button onClick={openPortal}>
-                <FiSettings />
-              </button>
-            )}
-          </IconContext.Provider>
-        </div>
-        <Portal isOpen={isOpen}>
-          <Settings
-            className="absolute right-0 top-0 h-full w-screen bg-white p-5 shadow shadow-gray-700 dark:bg-dark dark:text-neutral-200 sm:w-[440px]"
-            onClose={closePortal}
-          />
-        </Portal>
-      </>
-    </div>
+        <>
+          <div className="absolute right-0 top-0 m-6 flex gap-4">
+            <IconContext.Provider value={{ size: "2em", className: "dark:text-neutral-200" }}>
+              <button onClick={toggleAppTheme}>{isDarkModeEnabled ? <FiSun /> : <FiMoon />}</button>
+              <button onClick={toggleSound}>{isSoundEnabled ? <FiBell /> : <FiBellOff />}</button>
+              {timerStatus === PomodoroStatus.UNSTARTED && (
+                <button onClick={openPortal}>
+                  <FiSettings />
+                </button>
+              )}
+            </IconContext.Provider>
+          </div>
+          <Portal isOpen={isOpen}>
+            <Settings
+              className="absolute right-0 top-0 h-full w-screen bg-white p-5 shadow shadow-gray-700 dark:bg-dark dark:text-neutral-200 sm:w-[440px]"
+              onClose={closePortal}
+            />
+          </Portal>
+        </>
+      </div>
+    </>
   );
 }
