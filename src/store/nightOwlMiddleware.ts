140bf050fa8b460b63aa09ba528469f88a382975
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-06-18 04:05:59 +0200

    fix: Progress is reset after reaching midnight

--- src/store/nightOwlMiddleware.ts
+++ src/store/nightOwlMiddleware.ts
@@ -2,7 +2,7 @@ import { createListenerMiddleware } from "@reduxjs/toolkit";
 import { AppStartListening } from "@/store/store";
 import { Temporal } from "@js-temporal/polyfill";
 import { setNextMidnight } from "@/appSlice";
-import { finished } from "@/features/Timer/timerSlice";
+import { finished, resetProgress } from "@/features/Timer/timerSlice";
 import { getNextMidnight } from "@/store/helpers";
 
 const LS_NEXT_MIDNIGHT_KEY = "next_midnight";
@@ -19,14 +19,16 @@ startAppListening({
   actionCreator: finished,
   effect: (action, listenerApi) => {
     const state = listenerApi.getState();
-    const nextMidnight = Temporal.PlainDateTime.from(state.app.nextMidnight);
-    const offset = Temporal.PlainTime.from(state.settings.startNewDayAt);
 
+    const nextMidnight = Temporal.PlainDateTime.from(state.app.nextMidnight);
     const now = Temporal.Now.plainDateTime("gregory");
+
     if (Temporal.PlainDateTime.compare(nextMidnight, now) < 0) {
+      const offset = Temporal.PlainTime.from(state.settings.startNewDayAt);
       const nextMidnight = getNextMidnight(offset.hour, offset.minute);
 
       listenerApi.dispatch(setNextMidnight(nextMidnight));
+      listenerApi.dispatch(resetProgress());
     }
   },
 });
