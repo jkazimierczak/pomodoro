fc2f27745559fc445f5fa499b25019b1348c3368
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-06-27 14:19:41 +0200

    style: Input text is now centered

--- src/features/Settings/Inputs/InputNumber.tsx
+++ src/features/Settings/Inputs/InputNumber.tsx
@@ -4,11 +4,11 @@ interface InputNumberProps extends ComponentProps<"input"> {}
 
 export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(({ ...props }, ref) => {
   return (
-    <div className="w-9 rounded bg-neutral-200 py-0.5 text-neutral-100 dark:bg-neutral-800">
+    <div className="w-10 rounded bg-neutral-200 py-0.5 text-neutral-100 dark:bg-neutral-800">
       <input
         type="number"
         ref={ref}
-        className="border-none bg-transparent p-0 text-center text-neutral-900 [appearance:textfield] focus:ring-0 dark:text-neutral-100 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+        className="w-full border-none bg-transparent p-0 text-center text-neutral-900 [appearance:textfield] focus:ring-0 dark:text-neutral-100 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
         {...props}
       />
     </div>
