9168f5df9a2119013ffca25f343a9439a31d15b7
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-07-02 12:43:44 +0200

    chore: Remove react-buddy files

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "./index.css";
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "@/dev";
 import { store } from "@/app/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </DevSupport>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
