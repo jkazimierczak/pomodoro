c387377a7ded5db00754bbfbb575cf37449bf1e4
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-07-06 17:10:07 +0200

    build(lint): Added ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended"
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        ".eslintrc.{js,cjs}"
+      ],
+      parserOptions: {
+        sourceType: "script"
+      }
+    }
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "simple-import-sort"
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+    "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": "error",
+  }
+};
